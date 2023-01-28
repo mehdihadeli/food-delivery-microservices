da7b42ed77ac6b364ad9dffc6c42aadf6c2ae15c
mehdihadeli <mehdi.hadeli@outlook.com>
2023-01-28 23:40:21 +0330

    ci: update ci

--- back-merge.sh
+++ back-merge.sh
@@ -2,18 +2,19 @@
 
 # https://github.com/saitho/semantic-release-backmerge/issues/43
 # https://github.com/saitho/semantic-release-backmerge/issues/42
-# https://codewithhugo.com/fix-git-failed-to-push-updates-were-rejected/  
-
+# https://codewithhugo.com/fix-git-failed-to-push-updates-were-rejected/
 if [ $GITHUB_REF_NAME = 'main' ]
 # backmerge main to dev
 then git checkout dev
-    git merge origin/main -Xours
+    git merge origin/main -Xours --no-commit
+    git commit -m "chore(release): 🔧 preparations for next release with a backmerge [skip ci]" --author="github-actions[bot] <github-actions[bot]@users.noreply.github.com>"
     git push origin dev
 
 # backmerge dev to all feature branches: list all remote feature branches that have not been merged to any branches and has remote branch(-r)
 for branch in $(git branch -r --no-merged | grep "origin/feat/" | awk '{print $1}'| sed 's/^origin\///'); do
     git checkout $branch
-    git merge origin/dev -Xours
+    git merge origin/dev -Xours --no-commit
+    git commit -m "chore(release): 🔧 preparations for next release with a backmerge [skip ci]" --author="github-actions[bot] <github-actions[bot]@users.noreply.github.com>"
     git push origin $branch
 done
 fi

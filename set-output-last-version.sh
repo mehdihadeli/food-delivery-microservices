8b5f0195ba05f2dfd4c9c7b48d037a3db36c21c8
mehdihadeli <mehdi.hadeli@outlook.com>
2023-02-07 19:15:36 +0330

    chore: update ci

--- set-output-last-version.sh
+++ set-output-last-version.sh
@@ -7,11 +7,7 @@
 #https://medium.com/@gpanga/automating-releases-of-net-sdks-using-semantic-release-e3df46013876
 #https://github.com/Gabrielpanga/dotnet-nuget-example/blob/main/updateVersion.sh
 
-echo "semantic_nextRelease_version=$1" >> $GITHUB_OUTPUT
-echo "semantic_nextRelease_channel=$2" >> $GITHUB_OUTPUT
-echo "semantic_nextRelease_gitHead=$3" >> $GITHUB_OUTPUT
-echo "semantic_nextRelease_gitTag=$4" >> $GITHUB_OUTPUT
-echo "semantic_lastRelease_version=$5" >> $GITHUB_OUTPUT
-echo "semantic_lastRelease_channel=$6" >> $GITHUB_OUTPUT
-echo "semantic_lastRelease_gitHead=$7" >> $GITHUB_OUTPUT
-echo "semantic_lastRelease_gitTag=$8" >> $GITHUB_OUTPUT
+echo "semantic_lastRelease_version=$1" >> $GITHUB_OUTPUT
+echo "semantic_lastRelease_channel=$2" >> $GITHUB_OUTPUT
+echo "semantic_lastRelease_gitHead=$3" >> $GITHUB_OUTPUT
+echo "semantic_lastRelease_gitTag=$4" >> $GITHUB_OUTPUT

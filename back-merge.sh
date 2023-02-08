538c9aaa9a4430333505b40ad55509538c4a8171
mehdihadeli <mehdi.hadeli@outlook.com>
2023-02-09 01:46:00 +0330

    ci: update ci

--- back-merge.sh
+++ back-merge.sh
@@ -6,8 +6,8 @@
 
 # $1 is base-branch
 
-# back-merge base-branch to all feature branches: list all remote feature branches that have not been merged to any branches and has remote branch(-r)
-for branch in $(git branch -r --no-merged | grep "origin/feat/" | awk '{print $1}'| sed 's/^origin\///'); do
+# back-merge base-branch to all feature branches: list all remote feature branches that are not a pull request
+for branch in $(git branch -r --merged | grep "origin/feat/" | awk '{print $1}'| sed 's/^origin\///'); do
     git checkout $branch
     git merge origin/$1 -Xours --no-commit
     git commit -m "chore(release): 🔧 preparations for next release with a back-merge [skip ci]" --author="github-actions[bot] <github-actions[bot]@users.noreply.github.com>"

0f94bea1e013b54e2a27ae9635cd59f8515705cf
mehdihadeli <mehdi.hadeli@outlook.com>
2023-02-10 17:44:09 +0330

    ci: some ci changes

--- update-version.sh
+++ update-version.sh
@@ -2,6 +2,14 @@
 
 # update package version and version for in the csproj file to new version and commit it again with using https://github.com/semantic-release/git plugin
 # https://unix.stackexchange.com/questions/50313/how-do-i-perform-an-action-on-all-files-with-a-specific-extension-in-subfolders
-find . -name '*.Packages.props' -exec sed -i "s#<ApplicationVersion>.*#<ApplicationVersion>$1</ApplicationVersion>#" {} \;  -exec cat {} \;
-find . -name '*.Packages.props' -exec sed -i "s#<InformationalVersion>.*#<InformationalVersion>$1</InformationalVersion>#" {} \;  -exec cat {} \;
-find . -name '*.Packages.props' -exec sed -i "s#<Version>.*#<Version>$1</Version>#" {} \;  -exec cat {} \;
+new_version=$1
+for file in *.Packages.props; do
+  current_version=$(grep -oP '(?<=<Version>)[^<]+' "$file")
+  if [ "$current_version" != "$new_version" ]; then
+    sed -i "s#<ApplicationVersion>.*#<ApplicationVersion>$new_version</ApplicationVersion>#" "$file"
+    sed -i "s#<InformationalVersion>.*#<InformationalVersion>$new_version</InformationalVersion>#" "$file"
+    sed -i "s#<Version>.*#<Version>$new_version</Version>#" "$file"
+  else
+    echo application verion not changed
+  fi
+done

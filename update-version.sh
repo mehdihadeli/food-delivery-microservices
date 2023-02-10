43d2cb17087afa87303ef9be25199d3b88ae0242
mehdihadeli <mehdi.hadeli@outlook.com>
2023-02-10 19:11:13 +0330

    chore: update ci

--- update-version.sh
+++ update-version.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# update package version and version for in the csproj file to new version and commit it again with using https://github.com/semantic-release/git plugin
+# update package version and version for in the csproj file to new version
 # https://unix.stackexchange.com/questions/50313/how-do-i-perform-an-action-on-all-files-with-a-specific-extension-in-subfolders
 new_version=$1
-for file in *.Packages.props; do
+for file in $(find . -name "*.Packages.props" -type f); do
   current_version=$(grep -oP '(?<=<Version>)[^<]+' "$file")
   if [ "$current_version" != "$new_version" ]; then
     sed -i "s#<ApplicationVersion>.*#<ApplicationVersion>$new_version</ApplicationVersion>#" "$file"
